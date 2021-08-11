b2c864909f28c59dd0b7520f60fdd83c9fbb7d15
Abiyasa <abiyasa2410@gmail.com>
2021-08-11 21:07:48 +0700

    refactor: build using webpack

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,16 @@
-
-import {mongoAtlasOptions, mongoAtlasUri} from '@infrastructure/configs/mongo-atlas.config';
-import {USER_MONGOOSE_ENTITY, UserMongooseEntity} from '@modules/user/database/user.mongoose-entity';
-import {UserMongooseRepository} from '@modules/user/database/user.mongoose-repository';
-import {UserModule} from '@modules/user/user.module';
-import {Module} from '@nestjs/common';
-import {EventEmitterModule} from '@nestjs/event-emitter';
-import {MongooseModule, SchemaFactory} from '@nestjs/mongoose';
+import { mongoAtlasOptions, mongoAtlasUri } from '@infrastructure/configs/mongo-atlas.config';
+import { UserModule } from '@modules/user/user.module';
+import { Module } from '@nestjs/common';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     MongooseModule.forRoot(mongoAtlasUri, mongoAtlasOptions),
-    MongooseModule.forFeature([
-      {
-        name: USER_MONGOOSE_ENTITY,
-        schema: SchemaFactory.createForClass(UserMongooseEntity),
-      },
-    ]),
     EventEmitterModule.forRoot(),
     UserModule,
   ],
   controllers: [],
-  providers: [UserMongooseRepository],
+  providers: [],
 })
 export class AppModule {}
